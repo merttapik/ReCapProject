d27668eb13f25e1a4dddf7f636647fdb87612b5f
merttapik <78278144+merttapik@users.noreply.github.com>
2022-07-03 00:21:41 +0300

    Add project files.

---
+++ DataAccess/Concrete/InMemory/InMemoryCarDal.cs
@@ -0,0 +1,61 @@
+﻿using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace DataAccess.Concrete.InMemory
+{
+   public class InMemoryCarDal : ICarDal
+    {
+        List<Car> _cars;
+        public InMemoryCarDal()
+        {
+            //oracle,sql server,mongodb
+            _cars = new List<Car> { 
+              new Car{Id=1,BrandId=1,ColorId=1,DailyPrice=10,Description="nice car",ModelYear=2000},
+              new Car{Id=2,BrandId=1,ColorId=1,DailyPrice=10,Description="nice car",ModelYear=2000}
+            };
+        }
+        public void Add(Car car)
+        {
+            _cars.Add(car);
+        }
+
+        public void Delete(Car car)
+        {
+            Car carToDelete = _cars.SingleOrDefault(c=>c.Id==car.Id);
+            _cars.Remove(carToDelete);
+        }
+
+        public Car Get(Expression<Func<Car, bool>> filter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Car> GetAll()
+        {
+            return _cars;
+        }
+
+        public List<Car> GetAll(Expression<Func<Car, bool>> filter = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Car> GetById(int brandId)
+        {
+            //brandıd ye göre alıp where le onları arraya atıyo sondaki tolist ise list haline getiriyor
+           return _cars.Where(c => c.Id == brandId).ToList();
+        }
+
+        public void Update(Car car)
+        {
+            Car carToUpdate = _cars.SingleOrDefault(c => c.Id == car.Id);
+            carToUpdate.ModelYear = car.ModelYear;
+            //buraya diğer yerleri eşitle
+        }
+    }
+}
